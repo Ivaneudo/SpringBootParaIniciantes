32783ab588d7b32526b70efbf4959ac83dff45b2
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-03 20:32:35 -0300

    feat: Iniciando o projeto.

---
+++ src/main/java/com/cursoyt/api_spring_boot_iniciantes/ApiSpringBootIniciantesApplication.java
@@ -0,0 +1,13 @@
+package com.cursoyt.api_spring_boot_iniciantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootIniciantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootIniciantesApplication.class, args);
+	}
+
+}
