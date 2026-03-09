3fc20009b6cc5ed14414c4bebd503b233f911f96
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-08 23:28:28 -0300

    feat: Criando um objeto global para exceptions

---
+++ src/main/java/com/cursoyt/api_spring_boot_iniciantes/exceptions/GlobalExceptionHendler.java
@@ -0,0 +1,38 @@
+package com.cursoyt.api_spring_boot_iniciantes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHendler {
+  @ExceptionHandler(RecursoNaoEncontradoException.class)
+  public ResponseEntity<Object> handleRecursoNaoEncontrado(RecursoNaoEncontradoException ex) {
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDateTime.now());
+    body.put("status", HttpStatus.NOT_FOUND.value());
+    body.put("Error", "Recurso nao encontrado");
+    body.put("message", ex.getMessage());
+
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(RecursoNaoEncontradoException.class)
+  public ResponseEntity<Object> handleGenericException(Exception ex) {
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDateTime.now());
+    body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+    body.put("Error", "Erro interno do Servidor");
+    body.put("message", ex.getMessage());
+
+    return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+}
