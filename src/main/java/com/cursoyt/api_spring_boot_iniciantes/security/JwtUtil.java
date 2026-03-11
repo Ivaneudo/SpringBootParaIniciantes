71b356b00ebc6bc9db6cc272c0e3e55dfaed09e4
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-11 13:20:10 -0300

    feat: Adicionando o security no projeto.

---
+++ src/main/java/com/cursoyt/api_spring_boot_iniciantes/security/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.cursoyt.api_spring_boot_iniciantes.security;
+
+import com.cursoyt.api_spring_boot_iniciantes.model.Usuario;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+  private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+  private static final long EXPIRATION_TIME = 86400000;
+
+  public static String generateToken(String userName) {
+    return Jwts.builder()
+            .setSubject(userName)
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact();
+  }
+
+  public static String extractUsername(String token) {
+     return Jwts.parser()
+             .setSigningKey(key)
+             .build().parseClaimsJws(token)
+             .getBody().getSubject();
+  }
+
+  public static boolean validateToken(String token) {
+    try {
+      Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+      return true;
+    } catch (JwtException e) {
+      return false;
+    }
+  }
+}
